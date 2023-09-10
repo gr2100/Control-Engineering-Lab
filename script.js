cdf8e879f9e18ea7bc1e94e4e8f436d914383d2d
HarishKarthik0376 <103718125+HarishKarthik0376@users.noreply.github.com>
2023-09-10 12:41:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function result()
         score++;
     }
     alert("Your Score is:"+score+"/5")
+
     {
         document.getElementById("butsub1").style.display="inline";
     }
@@ -122,3 +123,97 @@ function result()
         document.getElementById('incorrectanswer15').style.display='block';
     }
     }
+    const start = 15;
+    let time = start*60;
+    let re=setInterval(runcountdown,1000);
+    const countdown = document.getElementById('countdown');
+    let prelab = document.getElementById('prelabtest');
+    let postlab = document.getElementById('postlabtest');
+    var sub = document.getElementById('buttest');
+    document.getElementById('buttest').addEventListener('click', function()
+    {   alert("Your Prelab Test Scores Have Been Submitted.");
+    
+        document.getElementById('prelabtest').style.display = "none";
+        document.getElementById("postlabtest").style.display = "block"
+        {
+            var posttimer = document.getElementById('countdown1');
+            const start = 15;
+            let time = start*60;
+            let pre = setInterval(postruncountdown,1000);
+            function postruncountdown()
+            {
+                const minutes = Math.floor(time/60);
+            let seconds = time%60;
+            seconds = seconds<10 ? '0'+seconds : seconds;
+            posttimer.innerHTML = `${minutes}: ${seconds}`;
+            time--;
+            if(time<0)
+        {
+            clearInterval(pre)
+            alert("Times Up!! Your Response Has Been Automatically Submited.");
+            window.open('home.html',"_parent");
+            }
+        }
+        }
+        
+    })
+    function result1() 
+    { 
+    var score=0;
+    if(document.getElementById("corr1").checked)
+    {
+        score++;
+    }
+    if(document.getElementById("corr2").checked)
+    {
+        score++;
+    }
+    if(document.getElementById("corr3").checked)
+    {
+        score++;
+    }
+    if(document.getElementById("corr4").checked)
+    {
+        score++;
+    }
+    if(document.getElementById("corr5").checked)
+    {
+        score++;
+    }
+     alert("Your Response Has Been Submited.");
+        window.open("home.html");
+    }
+    function runcountdown()
+    {
+        const minutes = Math.floor(time/60);
+        let seconds = time%60;
+        seconds = seconds<10 ? '0'+seconds : seconds;
+        countdown.innerHTML = `${minutes}: ${seconds}`;
+        time--;
+        if(time<0)
+        {
+            clearInterval(re)
+            prelab.style.display='none';
+            postlab.style.display = 'block';
+            var posttimer = document.getElementById('countdown1');
+            const start = 15;
+            let time = start*60;
+            let pre = setInterval(postruncountdown,1000);
+            function postruncountdown()
+            {
+                const minutes = Math.floor(time/60);
+            let seconds = time%60;
+            seconds = seconds<10 ? '0'+seconds : seconds;
+            posttimer.innerHTML = `${minutes}: ${seconds}`;
+            time--;
+            if(time<0)
+        {
+            clearInterval(pre)
+            }
+        }
+
+    }
+
+    
+    }
+    
